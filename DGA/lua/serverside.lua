1a692a57816f7c4d048cccbd8dede4b40817ab9d
dgrawns <dgrawns@gmail.com>
2016-09-04 00:47:22 -0400

    Added includes()'s and CS Files.

---
+++ DGA/lua/serverside.lua
@@ -0,0 +1,8 @@
+AddCSLuaFile("autorun/dga.lua")
+AddCSLuaFile("shared.lua")
+AddCSLuaFile("clientside.lua")
+
+include("shared.lua")
+include("commands.lua")
+include("config.lua")
+include("teams.lua")
