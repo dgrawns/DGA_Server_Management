d25a5302e43dbf1e643b0e1c56dfb51663e0c73e
dgrawns <dgrawns@gmail.com>
2016-09-04 00:55:53 -0400

    Update serverside.lua

--- DGA/lua/serverside.lua
+++ DGA/lua/serverside.lua
@@ -3,6 +3,7 @@ AddCSLuaFile("shared.lua")
 AddCSLuaFile("clientside.lua")
 
 include("shared.lua")
-include("commands.lua")
+include("console_commands.lua")
+include("chat_commands.lua")
 include("config.lua")
 include("teams.lua")
