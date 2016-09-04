a21c3582e23caa9ab55cd5fbe64f587da2d09fc1
dgrawns <dgrawns@gmail.com>
2016-09-04 12:05:36 -0400

    Update hud.lua

--- DGA/lua/hud.lua
+++ DGA/lua/hud.lua
@@ -1,1 +1,8 @@
+function DGA_HUD()
+  //Name
+  draw.SimpleText(LocalPlayer():Nick(), "DermaDefault", 10, 200, team.GetColor(LocalPlayer():Team()), TEXT_ALIGN_LEFT, TEXT_ALIGN_BOTTOM)
+  surface.SetDrawColor(0, 0, 0, 150)
+  surface.DrawRect(6, 200, 100, 300)
+end
 
+hook.Add("HUDPaint", "DGA_HUD", DGA_HUD())
