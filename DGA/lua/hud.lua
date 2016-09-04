ddc485348d81ad0c48a6dd4998ba247b6f465789
dgrawns <dgrawns@gmail.com>
2016-09-04 12:22:54 -0400

    Delete hud.lua

--- DGA/lua/hud.lua
+++
@@ -1,8 +0,0 @@
-function DGA_HUD()
-  //Name
-  draw.SimpleText(LocalPlayer():Nick(), "DermaDefault", 100, 800, team.GetColor(LocalPlayer():Team()), TEXT_ALIGN_LEFT, TEXT_ALIGN_BOTTOM)
-  surface.SetDrawColor(0, 0, 0, 150)
-  surface.DrawRect(6, 200, 100, 300)
-end
-
-hook.Add("HUDPaint", "DGA_HUD", DGA_HUD())
