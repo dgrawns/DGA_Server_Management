589744c64bcafcdda957042deeb228abed2037fa
dgrawns <dgrawns@gmail.com>
2016-09-04 12:22:40 -0400

    Forced to merge hud into clientside.lua.

--- DGA/lua/clientside.lua
+++ DGA/lua/clientside.lua
@@ -1,2 +1,10 @@
-include("hud.lua")
 include("shared.lua")
+
+//HUD
+function DGA_HUD()
+  draw.SimpleText(LocalPlayer():Nick(), "DermaDefault", 100, 800, team.GetColor(LocalPlayer():Team()), TEXT_ALIGN_LEFT, TEXT_ALIGN_BOTTOM)
+  surface.SetDrawColor(0, 0, 0, 150)
+  surface.DrawRect(6, 200, 100, 300)
+end
+
+hook.Add("HUDPaint", "DGA_HUD", DGA_HUD())
