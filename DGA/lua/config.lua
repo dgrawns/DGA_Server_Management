642230aba488e68ade37125b3791707c21be190b
dgrawns <dgrawns@gmail.com>
2016-09-05 19:37:08 -0400

    Update config.lua

---
+++ DGA/lua/config.lua
@@ -0,0 +1,34 @@
+// Please list in order from top to bottom.
+Ranks = {
+"owner",
+"co-owner",
+"head-admin",
+"super-admin",
+"admin",
+"mod",
+"respected",
+"member",
+"user"
+}
+Teams = {
+"Owner",
+"Co-Owner",
+"Head Admin",
+"Super Admin",
+"Admin",
+"Moderator",
+"Respected",
+"Member",
+"Guest"
+}
+Colors = {
+"0, 0, 0",
+"0, 255, 255",
+"128, 0, 128",
+"255, 0, 0",
+"0, 0, 255",
+"0, 255, 0",
+"255, 255, 0",
+"0, 0, 128",
+"150, 150, 150"
+}
