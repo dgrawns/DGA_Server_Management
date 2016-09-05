51683480ac74bf1c2e6d1f326221dad9964d2538
dgrawns <dgrawns@gmail.com>
2016-09-05 18:42:16 -0400

    Update clientside.lua

--- DGA/lua/clientside.lua
+++ DGA/lua/clientside.lua
@@ -1,10 +1,1 @@
 include("shared.lua")
-
-//HUD
-function DGA_HUD()
-  draw.SimpleText(LocalPlayer():GetName(), "DermaDefault", 100, 800, team.GetColor(LocalPlayer():Team()), TEXT_ALIGN_LEFT, TEXT_ALIGN_BOTTOM)
-  surface.SetDrawColor(0, 0, 0, 150)
-  surface.DrawRect(6, 200, 100, 300)
-end
-
-hook.Add("HUDPaint", "DGA_HUD", DGA_HUD())
